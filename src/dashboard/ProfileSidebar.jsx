3006add503ba7887db7cdfe4df857702320dd658
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-05-24 13:20:11 +0300

    add conditional statements to toggle between showing the entire sidebar, and just a part of it

--- src/dashboard/ProfileSidebar.jsx
+++ src/dashboard/ProfileSidebar.jsx
@@ -1,18 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ProfileContext } from "../context/ProfileContext";
 
 const ProfileSidebar = () => {
-  const {profiles, selectedProfile} = useContext(ProfileContext)
+  const { profiles, selectedProfile } = useContext(ProfileContext);
+  const [sidebarVisible, setSidebarVisible] = useState(true);
 
-    // Find the detailed information of the selected profile
-    const profileDetails = profiles.find(profile => profile.id === selectedProfile);
 
-    // Check if a profile is selected
-    if (!profileDetails) {
-        return <div>No profile selected</div>;
-    }
+  // Find the detailed information of the selected profile
+  const profileDetails = profiles.find(
+    (profile) => profile.id === selectedProfile
+  );
+
+  // Check if a profile is selected
+  if (!profileDetails) {
+    return <div>No profile selected</div>;
+  }
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
 
   return (
+    <>
+     {sidebarVisible && (
     <aside className="bg-white w-96 h-full p-4">
       {/* Header with weather, notifications and country */}
       <nav>
@@ -42,19 +52,35 @@ const ProfileSidebar = () => {
               <path d="M221.8,175.94C216.25,166.38,208,139.33,208,104a80,80,0,1,0-160,0c0,35.34-8.26,62.38-13.81,71.94A16,16,0,0,0,48,200H88.81a40,40,0,0,0,78.38,0H208a16,16,0,0,0,13.8-24.06ZM128,216a24,24,0,0,1-22.62-16h45.24A24,24,0,0,1,128,216ZM48,184c7.7-13.24,16-43.92,16-80a64,64,0,1,1,128,0c0,36.05,8.28,66.73,16,80Z"></path>
             </svg>
           </span>
+          <span className="justify-end rounded-full bg-white shadow p-1" onClick={toggleSidebar}>
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="#9ca3af"
+          viewBox="0 0 256 256"
+        >
+          <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
+        </svg>
+        </span>
         </div>
+       
       </nav>
       <div className="flex flex-col items-center mt-5">
         {/* Profile Image */}
         <div>
-            <img
-                src={profileDetails.profileImage}
-                alt={`${profileDetails.name}-profile-image`}
-                className="rounded-full w-32 h-32"
-            />
-            {/* profile details */}
-            <p className="font-bold mt-4">{profileDetails.name}</p>
-            <p className="text-gray-500">{profileDetails.jobTitle}</p>
+          <img
+            src={profileDetails && profileDetails.profileImage}
+            alt={`${profileDetails && profileDetails.name}-profile-image`}
+            className="rounded-full w-32 h-32"
+          />
+          {/* profile details */}
+          <p className="font-bold mt-4 text-center">
+            {profileDetails && profileDetails.name}
+          </p>
+          <p className="text-gray-500 text-center">
+            {profileDetails && profileDetails.jobTitle}
+          </p>
         </div>
         {/* Audio and Video call buttons */}
         <div id="buttons" className="flex mt-4 space-x-4">
@@ -104,7 +130,21 @@ const ProfileSidebar = () => {
 
         <p className="text-violet-600 font-bold text-center p-4">View More</p>
       </div>
-    </aside>
+    ) : (
+      <span className="justify-end rounded-full bg-white shadow p-1" onClick={toggleSidebar}>
+           <svg
+           xmlns="http://www.w3.org/2000/svg"
+           width="20"
+           height="20"
+           fill="#9ca3af"
+           viewBox="0 0 256 256"
+         >
+           <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
+         </svg>
+         </span>
+         </aside>
+    )}
+    </>
   );
 };
 
